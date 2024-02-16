9f6891c2f9d7261395c97de8631fa3b0dd1c5c55
EdwinUnity3D <eapaniagua8@misena.edu.co>
2024-02-16 14:36:05 -0500

    Creado de nuevo el player controller con el animator controller

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -7,29 +8,64 @@ public class PlayerController : MonoBehaviour
     private Animator _animator;
 
     private const string MOVE_HANDS = "Move Hands";
-    private bool isMovingHangs;
-    
+    private bool movingHands;
+
+
+    private const string MOVE_X = "Move_X";
+    private const string MOVE_Y = "Move_Y";
+
+    private bool isMoving;
+    private const string IS_MOVING = "isMoving";
+
+
+
+    private float Move_X;
+    private float Move_Y;
     
     // Start is called before the first frame update
     void Start()
     {
-
         _animator = GetComponent<Animator>();
-        _animator.SetBool(MOVE_HANDS, isMovingHangs);
+        _animator.SetBool(MOVE_HANDS, movingHands);
         
-
+        _animator.SetBool(IS_MOVING, isMoving);
+        
+        _animator.SetFloat(MOVE_X, Move_X);
+        _animator.SetFloat(MOVE_Y, Move_Y);
+        
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        Move_X = Input.GetAxis("Horizontal");
+        Move_Y = Input.GetAxis("Vertical");
         
         
-        
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Mathf.Sqrt(Move_X * Move_X + Move_Y * Move_Y) > 0.01)
         {
-            isMovingHangs = !isMovingHangs;
-            _animator.SetBool(MOVE_HANDS, isMovingHangs);
+          
+            _animator.SetBool(IS_MOVING, isMoving = true);
+            _animator.SetFloat(MOVE_X, Move_X);
+            _animator.SetFloat(MOVE_Y, Move_Y);
+            
         }
+        else
+        {
+            _animator.SetBool(IS_MOVING, isMoving = false);
+        }
+        
+        
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            movingHands = !movingHands;
+            
+            _animator.SetBool(MOVE_HANDS, movingHands);
+
+
+        }
+        
     }
 }
