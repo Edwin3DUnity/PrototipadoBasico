f6c8bdd4ee98a55d3a35e4086ded6276dd8ac7a5
EdwinUnity3D <eapaniagua8@misena.edu.co>
2024-03-13 13:27:16 -0500

    Se crea el player controller y se configuran las animaciones con el blend tree y demas

--- Assets/_Scripts/ControladorPersonaje.cs
+++ Assets/_Scripts/ControladorPersonaje.cs
@@ -1,22 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Search;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class ControladorPersonaje : MonoBehaviour
 {
-    private Animator _animator;
-    
-    private const string IS_MOVING_HAND = "isMovingHand";
-    private const string IS_MOVING = "isMoving";
-    private const string HORIZONTAL ="horizontal";
-    private const string VERTICAL = "vertical";
 
-    [SerializeField] private bool isMovingHand;
-    [SerializeField] private bool isMoving;
+    private Animator _animator;
+
+    private const string IS_MOVING_HAND = "isMovingHand";
+
+
+    public bool isMovingHand;
+    
+    private const string IS_MOVING_BODY ="isMovingBody";
+    private const string HORIZONTAL ="Horizontal";
+    private const string VERTICAL = "Vertical";
+
+    private bool isMovingBody;
 
     private float horizontal;
     private float vertical;
-    
     
     // Start is called before the first frame update
     void Start()
@@ -24,51 +28,49 @@ public class PlayerController : MonoBehaviour
         _animator = GetComponent<Animator>();
         
         _animator.SetBool(IS_MOVING_HAND, isMovingHand);
-        _animator.SetBool(IS_MOVING, isMoving);
+        _animator.SetBool(IS_MOVING_BODY, isMovingBody);
         
         _animator.SetFloat(HORIZONTAL, horizontal);
         _animator.SetFloat(VERTICAL, vertical);
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        MoviendoManos();
-        MoviendoCuerpo();
+     MoverMano();   
+     MoverCuerpo();
+    }
+
+    private void MoverMano()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isMovingHand = !isMovingHand;
+            
+            _animator.SetBool(IS_MOVING_HAND, isMovingHand);
+
+        }
         
     }
 
-    private void MoviendoManos()
-    {
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            isMovingHand = !isMovingHand;
-            _animator.SetBool(IS_MOVING_HAND, isMovingHand);
-
-
-        }
-    }
-
-
-    private void MoviendoCuerpo()
+    private void MoverCuerpo()
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
 
         if (Mathf.Sqrt(horizontal * horizontal + vertical * vertical) >= 0.01)
         {
-            _animator.SetBool(IS_MOVING, isMoving = true);
+            _animator.SetBool(IS_MOVING_BODY, isMovingBody = true);
             _animator.SetFloat(HORIZONTAL, horizontal);
             _animator.SetFloat(VERTICAL, vertical);
-            _animator.SetBool(IS_MOVING_HAND, isMovingHand = false);
+            _animator.SetBool(IS_MOVING_HAND,isMovingHand = false);
         }
         else
         {
-            _animator.SetBool(IS_MOVING, isMoving = false);
+            _animator.SetBool(IS_MOVING_BODY, isMovingBody = false);
         }
         
-
-
+        
     }
 }
